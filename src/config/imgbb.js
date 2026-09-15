bfbece48b2006fd9ef865fc6c6cd98c1637f6904
Martin Fiori <mnf@jphlions.com>
2026-09-15 10:47:22 -0300

    Configuración imgbb

--- src/config/imgbb.js
+++ src/config/imgbb.js
@@ -2,7 +2,10 @@ const DEFAULT_TIMEOUT_MS = 15000
 const IMGBB_UPLOAD_URL = 'https://api.imgbb.com/1/upload'
 
 function getImgbbApiKey() {
+  console.log('-'.repeat(150))
+  console.log(process.env.IMGBB_API_KEY)
   const key = process.env.IMGBB_API_KEY
+  console.log(key)
   if (typeof key !== 'string') {
     return null
   }
